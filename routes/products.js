cfd2e1b975f567199c9efdd089695bead53e0459
NeshGogo <rafaelaguero10@hotmail.com>
2021-03-14 20:23:46 -0400

    layout with pug

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,12 @@ const products = [
   {
     name: 'Red shoes',
     price: 75,
+    image: 'https://i.pinimg.com/originals/48/ea/28/48ea28e4d4496c85ea17efaa5dd0cde3.jpg'
   },
   {
     name: 'Black bike',
     price: 200,
+    image: 'https://i1.adis.ws/i/washford/340910/Carrera-Vengeance-Mens-Mountain-Bike-2020---Black---XS-S-M-L-XL-Frames.webp?$sfcc_tile$&w=680'
   }
 ];
 
