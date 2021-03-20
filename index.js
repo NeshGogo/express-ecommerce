30d71e709c69076a4bc030b03450e5b08104a952
NeshGogo <rafaelaguero10@hotmail.com>
2021-03-19 21:59:01 -0400

    ordering the code

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const path = require('path');
 const bodyParse = require('body-parser');
-const productsRouter = require('./routes/products');
+const productsRouter = require('./routes/views/products');
 const productsApiRouter = require('./routes/api/products');
+
+// app
 const app = express();
 
+// middlewares
+app.use(express.json())
+
+// statics files
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
+// view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(express.json())
+// routes
 app.use('/products', productsRouter);
 app.use('/api/products', productsApiRouter)
 
+// redirect
+app.get('/', (req, res) => {
+  res.redirect('/products')
+})
 
+// server
 const server = app.listen(3000, () => {
   console.log(`listening http://localhost:${server.address().port}/`)
 })
